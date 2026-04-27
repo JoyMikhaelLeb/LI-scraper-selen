c32b40c4fbc6971e0f8ba36c24a4634d5da87269
Joy Mikhael <31731899+JoyMikhaelLeb@users.noreply.github.com>
2026-04-27 14:52:25 +0300

    updated

--- advanced_search_companies.py
+++ advanced_search_companies.py
@@ -48,7 +48,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate('spherical-list-284723-216944ab15f1.json')
+    cred = credentials.Certificate('key.json')
     default_app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
